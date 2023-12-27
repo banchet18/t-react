8d86fd464ff721f89d7a7429f122ed95671b1fc3
banchet18 <banjarac6@gmail.com>
2023-12-27 23:30:05 +0530

    c

--- reactlearn/src/components/Title.js
+++ reactlearn/src/components/Title.js
@@ -1,4 +1,10 @@
-const Title = () => {
-  return <div>Title</div>;
+const Title = ({ name, collegeName, rollNo }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{collegeName}</h2>
+      <h3>{rollNo}</h3>
+    </div>
+  );
 };
 export default Title;
