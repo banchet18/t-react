0d5f0ad3cbb9a9fefd845b1d1a1f60f768dd0cf7
banchet18 <banjarac6@gmail.com>
2023-12-27 23:17:05 +0530

    c

--- reactlearn/src/components/UnorderedList.js
+++ reactlearn/src/components/UnorderedList.js
@@ -1,4 +1,14 @@
 const UnorderedList = () => {
-  return <div>UnorderedList</div>;
+  return (
+    <div>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+        <li>item 4</li>
+        <li>item 5</li>
+      </ul>
+    </div>
+  );
 };
 export default UnorderedList;
