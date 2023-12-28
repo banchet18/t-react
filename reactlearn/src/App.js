8cae514db281e517adaf7ede33f8e637a22017c3
banchet18 <banjarac6@gmail.com>
2023-12-28 09:05:53 +0530

    c

--- reactlearn/src/App.js
+++ reactlearn/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import About from "./screens/About";
 import Contact from "./screens/Contact";
+import Register from "./screens/Register";
 
 function App() {
   return (
     <div>
-      <About />
+      {/* <About /> */ s}
+      <Register />
       {/* <Contact /> */}
     </div>
   );
