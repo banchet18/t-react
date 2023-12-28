67763c7f7bdd902039e9701d8fc9fb4f6e365194
banchet18 <banjarac6@gmail.com>
2023-12-28 09:38:03 +0530

    c

--- reactlearn/src/screens/UserList.js
+++ reactlearn/src/screens/UserList.js
@@ -1,4 +1,9 @@
 const UserList = () => {
+  let data = [
+    { name: "sham", collegeName: "pce", rollNo: 45 },
+    { name: "ragav", collegeName: "pillai", rollNo: 50 },
+    { name: "sameer", collegeName: "ztm", rollNo: 60 },
+  ];
   return <div>UserList</div>;
 };
 export default UserList;
