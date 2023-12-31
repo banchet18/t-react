5ef168aba76881eff0dace57592c87c6ce56accb
banchet18 <banjarac6@gmail.com>
2023-12-31 11:55:40 +0530

    c

--- reactlearn/src/screens/Registers.js
+++ reactlearn/src/screens/Registers.js
@@ -1,4 +1,28 @@
+import { useState } from "react";
+
 const Registers = () => {
-  return <div>Register2</div>;
+  const [count, setCount] = useState();
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  return (
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <div>
+        Count:{count}
+        <button onClick={increment}></button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </div>
+  );
 };
 export default Registers;
