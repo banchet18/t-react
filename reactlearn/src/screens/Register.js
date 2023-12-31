335edfacddde4381c0686783bfb9093ce35db382
banchet18 <banjarac6@gmail.com>
2023-12-31 11:19:58 +0530

    c

--- reactlearn/src/screens/Register.js
+++ reactlearn/src/screens/Register.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+
 const Register = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useEffect();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log("hello world");
+  };
+
+  console.log(email);
+
   return (
     <form>
       <div>
-        <input type="text" />
-        <button></button>
+        <input
+          type="email"
+          placeholder="enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button onClick={handleRegister}>Submit</button>
+        <input
+          type="password"
+          placeholder="enter password"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button onClick={handleRegister}>Submit</button>
       </div>
     </form>
   );
