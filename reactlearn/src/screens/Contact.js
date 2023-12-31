75f8fa5ac9b2a0622f21c3732fe5d8dcbc78d5b6
banchet18 <banjarac6@gmail.com>
2023-12-31 22:08:18 +0530

    c

--- reactlearn/src/screens/Contact.js
+++ reactlearn/src/screens/Contact.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { incrementByAmount } from "../store/reducer";
 import store from "../store/store";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   const value = useSelector((state) => state.value);
@@ -15,6 +16,7 @@ const Contact = () => {
       >
         This is contact screen
       </button>
+      <Link to="/register">link to register</Link>
     </div>
   );
 };
