ae3431b7f0b1f803feff0043c1f301da06f1dd1a
banchet18 <banjarac6@gmail.com>
2023-12-31 21:28:29 +0530

    c

--- reactlearn/src/store/reducer.js
+++ reactlearn/src/store/reducer.js
@@ -1,18 +1,13 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 
-const increment = createAction("counter/increment");
-const incrementByAmount = createAction("counter/incrementByAmount");
+const incrementByAmount = createAction("counter/incrementByAmount"); //Action
 
 const initialState = { value: 0 };
 
 const rootReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(increment, (state, action) => {
-      state.value++;
-    })
-    .addCase(incrementByAmount, (state, action) => {
-      state.value += action.payload;
-    });
+  builder.addCase(incrementByAmount, (state, action) => {
+    state.value += action.payload;
+  });
 });
 
 export default rootReducer;
