bf641b5ce8e3e52b19bad7a3b5490f43cf8f9845
banchet18 <banjarac6@gmail.com>
2023-12-31 21:54:42 +0530

    c

--- reactlearn/src/screens/Contact.js
+++ reactlearn/src/screens/Contact.js
@@ -1,17 +1,21 @@
+import { useSelector } from "react-redux";
 import { incrementByAmount } from "../store/reducer";
 import store from "../store/store";
 
 const Contact = () => {
-  store.dispatch(incrementByAmount(5));
+  const value = useSelector((state) => state.value);
   return (
-    <button
-      onClick={() => {
-        store.dispatch(incrementByAmount(5));
-        console.log("state updated");
-      }}
-    >
-      This is contact screen
-    </button>
+    <div>
+      <h1>{value}</h1>
+      <button
+        onClick={() => {
+          store.dispatch(incrementByAmount(5));
+          console.log("state updated");
+        }}
+      >
+        This is contact screen
+      </button>
+    </div>
   );
 };
 export default Contact;
