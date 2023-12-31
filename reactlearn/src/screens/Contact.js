df1e818ae2c46160a8b6bf68abb485324b5fde4f
banchet18 <banjarac6@gmail.com>
2023-12-31 21:40:43 +0530

    c

--- reactlearn/src/screens/Contact.js
+++ reactlearn/src/screens/Contact.js
@@ -1,4 +1,8 @@
+import { incrementByAmount } from "../store/reducer";
+import store from "../store/store";
+
 const Contact = () => {
+  store.dispatch(incrementByAmount);
   return <div>This is contact screen</div>;
 };
 export default Contact;
