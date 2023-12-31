7ada8b25d37afbd4333933209408c274f5f7a81d
banchet18 <banjarac6@gmail.com>
2023-12-31 18:31:22 +0530

    c

--- reactlearn/src/screens/Registers.js
+++ reactlearn/src/screens/Registers.js
@@ -19,14 +19,16 @@ const Registers = () => {
   };
 
   const addTodo = () => {
-    setTodos(() => {});
+    setTodos((t) => [...t, "new Todo"]);
   };
 
   return (
     <div>
       <div>
         <h2>My Todos</h2>
-        {todos.map((todo) => [...todo, "new todo"])}
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
         <button onClick={addTodo}>Add Todo</button>
       </div>
       <hr />
