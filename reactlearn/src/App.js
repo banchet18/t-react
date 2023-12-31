e6521d52244d6f3a5560cea33977d2c9b03ffcc3
banchet18 <banjarac6@gmail.com>
2023-12-31 18:53:30 +0530

    c

--- reactlearn/src/App.js
+++ reactlearn/src/App.js
@@ -3,6 +3,8 @@ import About from "./screens/About";
 import Contact from "./screens/Contact";
 import Registers from "./screens/Registers";
 import UserList from "./screens/UserList";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
