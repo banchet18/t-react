efaa958e93e446abd4a322e7affe7362bfafdf8b
banchet18 <banjarac6@gmail.com>
2024-01-01 09:11:06 +0530

    c

--- practice/src/screens/Home.js
+++ practice/src/screens/Home.js
@@ -1,23 +1,10 @@
-import { incrementByAmount } from "../store/Reduce";
 import Card from "../components/Card";
-import store from "../store/Store";
+
 import data from "../utils";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 
 const Home = () => {
-  const value = useSelector((state) => state.value);
   return (
     <div>
-      <h1>{value}</h1>
-      <button
-        onClick={() => {
-          store.dispatch(incrementByAmount(5));
-          console.log("state updated");
-        }}
-      >
-        add by 5
-      </button>
       <div>
         {data.map(({ title, email, rollNo }) => {
           return <Card title={title} email={email} rollNo={rollNo} />;
