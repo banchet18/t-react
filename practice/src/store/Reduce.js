94a4ea1d1fb5f945969bbf90ce71850bc2032536
banchet18 <banjarac6@gmail.com>
2024-01-01 08:06:32 +0530

    c

--- practice/src/store/Reduce.js
+++ practice/src/store/Reduce.js
@@ -1,13 +1,13 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
 const incrementByAmount = createAction("counter/incrementByAmount");
 
 const initialState = { value: 0 };
 
 const rootReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(increment, (state, action) => {
-      state.value++;
-    })
-    .addCase(incrementByAmount, (state, action) => {
-      state.value += action.payload;
-    });
+  builder.addCase(incrementByAmount, (state, action) => {
+    state.value += action.payload;
+  });
 });
+
+export { incrementByAmount, rootReducer };
