5e51fe7c6691ba7c0678d6dbf95d07b3f31537b9
banchet18 <banjarac6@gmail.com>
2024-01-01 08:41:33 +0530

    c

--- practice/src/screens/Home.js
+++ practice/src/screens/Home.js
@@ -1,4 +1,6 @@
+import { incrementByAmount } from "../../../reactlearn/src/store/reducer";
 import Card from "../components/Card";
+import store from "../store/Store";
 import data from "../utils";
 import { useSelector } from "react-redux";
 
@@ -6,9 +8,20 @@ const Home = () => {
   const value = useSelector((state) => state.value);
   return (
     <div>
-      {data.map(({ title, email, rollNo }) => {
-        return <Card title={title} email={email} rollNo={rollNo} />;
-      })}
+      <h1>{value}</h1>
+      <button
+        onClick={() => {
+          store.dispatch(incrementByAmount(5));
+          console.log("state updated");
+        }}
+      >
+        add by 5
+      </button>
+      <div>
+        {data.map(({ title, email, rollNo }) => {
+          return <Card title={title} email={email} rollNo={rollNo} />;
+        })}
+      </div>
     </div>
   );
 };
