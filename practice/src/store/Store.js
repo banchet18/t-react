33ee8fba252274553e9ef7376f06482d4b257126
banchet18 <banjarac6@gmail.com>
2024-01-01 10:12:01 +0530

    c

--- practice/src/store/Store.js
+++ practice/src/store/Store.js
@@ -1,6 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./Reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const store = configureStore({ reducer: rootReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = configureStore({ reducer: rootReducer });
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
