eaa55fbcc1e56e5ce836ce64976d88f394f32ed1
banchet18 <banjarac6@gmail.com>
2024-01-01 07:55:05 +0530

    c

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,10 +1,18 @@
 // import Home from "./screens/Home";
 import Register from "./screens/Register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 const App = () => {
   return (
     <div>
-      <Register />
+      <RouterProvider router={router} />
     </div>
   );
 };
