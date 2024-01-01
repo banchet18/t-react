481f56d4919f5f3f83e561586b555dd35fcd766d
banchet18 <banjarac6@gmail.com>
2024-01-01 08:31:14 +0530

    c

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,8 +3,17 @@ import Register from "./screens/Register";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/Store";
+import Home from "./screens/Home";
 
 const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
   {
     path: "/register",
     element: <Register />,
