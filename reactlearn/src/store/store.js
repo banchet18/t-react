b9af8a390adfb37a2fd4b8acc82b778f2df7b7da
banchet18 <banjarac6@gmail.com>
2024-01-01 09:55:30 +0530

    c

--- reactlearn/src/store/store.js
+++ reactlearn/src/store/store.js
@@ -1,6 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const store = configureStore({ reducer: rootReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = configureStore({ reducer: persistedReducer });
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
