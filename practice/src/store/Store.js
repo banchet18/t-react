2386a7f37d5da32e01b8912d4793946301446610
banchet18 <banjarac6@gmail.com>
2024-01-01 08:04:46 +0530

    c

--- practice/src/store/Store.js
+++ practice/src/store/Store.js
@@ -1,7 +1,6 @@
 import rootReducer from "./Reduce";
 
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterReducer";
 
 const store = configureStore({
   reducer: rootReducer,
