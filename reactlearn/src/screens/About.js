4c6a36f24ded8a65cdeb56a824edbae81ed9cc78
banchet18 <banjarac6@gmail.com>
2024-01-01 12:02:15 +0530

    c

--- reactlearn/src/screens/About.js
+++ reactlearn/src/screens/About.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import Title from "../components/Title";
+import { CountContext } from "../context/countContext";
 // import UnorderedList from "../components/UnorderedList";
 
 const About = () => {
+  const value = useContext(CountContext);
   return (
     <div>
+      <h1>{value}</h1>
       <Title name={"lakhan"} collegeName={"pce"} rollNo={12} />
       {/* <UnorderedList /> */}
       <Title name={"sham"} collegeName={"pillai"} rollNo={15} />
