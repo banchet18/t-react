6bbd033696c5cdefd17fe1f9ca73761821942a2b
banchet18 <banjarac6@gmail.com>
2024-01-01 09:32:51 +0530

    c

---
+++ practice/src/store/Store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterReducer";
+
+const store = configureStore({ reducer: counterReducer });
+
+export default store;
