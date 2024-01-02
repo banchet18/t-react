c6d0aebc50aab287ce268103d40f28865c60fe11
banchet18 <banjarac6@gmail.com>
2024-01-02 12:59:05 +0530

    c

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <header className="bg-white dark:bg-gray-900  text-gray-900 dark:text-white">
       <nav className="flex justify-between items-center p-5">
-        <h1>Jarvis</h1>
+        <h1>Chetan Banjara</h1>
         <div className="flex justify-between">
           <MdNightsStay />
           <MdWbSunny />
