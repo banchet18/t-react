507f6d8263987feb7b02f6dd463bf68fe761fa62
banchet18 <banjarac6@gmail.com>
2024-01-02 12:50:07 +0530

    c

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,8 +1,13 @@
+import { MdNightsStay, MdWbSunny } from "react-icons/md";
 const Header = () => {
   return (
     <header>
       <nav>
         <h1>Jarvis</h1>
+        <div>
+          <MdNightsStay />
+          <MdWbSunny />
+        </div>
       </nav>
     </header>
   );
