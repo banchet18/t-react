154bcfa1fead65efaa41e44a21b4cfedb88af949
banchet18 <banjarac6@gmail.com>
2024-01-02 22:09:20 +0530

    c

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="toggleButton">
-        <button></button>
+        <button>
+          <ReorderIcon />
+        </button>
       </div>
       <div className="links">
         <Link to="/">Home</Link>
