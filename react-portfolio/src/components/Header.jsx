0bd1392edc99c83bad3e61057e791b4bf7f457f5
banchet18 <banjarac6@gmail.com>
2024-01-02 12:53:09 +0530

    c

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
       <nav className="flex justify-between">
         <h1>Jarvis</h1>
-        <div>
+        <div className="flex justify-between">
           <MdNightsStay />
           <MdWbSunny />
         </div>
