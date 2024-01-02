75ed6208866634ee7fe3e44d39e556c05405a072
banchet18 <banjarac6@gmail.com>
2024-01-02 14:20:15 +0530

    c

--- react-portfolio/src/components/Hero.jsx
+++ react-portfolio/src/components/Hero.jsx
@@ -9,10 +9,10 @@ const Hero = () => {
   ];
 
   return (
-    <section>
-      <h2>Chetan </h2>
-      <h3>Software engineer</h3>
-      <p>
+    <section className="min-h-screen flex-col justify-start items-center p-5 text-center">
+      <h2 className="text-5xl text-rose-600 uppercase font-bold">Chetan </h2>
+      <h3 className="py-3 text-2xl">Software engineer</h3>
+      <p className=" max-w-xl font-light text-gray-500">
         hello <span className="animate-pulse text-4xl">👋</span>
         welcome to site, I am a freelace software engineer. I love to work on
         web dev, Mobile dev & automation projects.
