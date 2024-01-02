8a7aec6d868575482b359b9a75320db67ce74770
banchet18 <banjarac6@gmail.com>
2024-01-02 13:11:27 +0530

    c

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div>
-      <Header />
+    <div className={darkMode && "dark"}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   );
 }
--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,12 +1,23 @@
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
-const Header = () => {
+const Header = ({ darkMode, setDarkMode }) => {
   return (
     <header className=" dark:bg-gray-900 text-gray-900 bg-white  dark:text-white">
       <nav className="flex justify-between items-center p-5">
-        <h1 className="text-4xl">Chetan Banjara</h1>
-        <div className="flex justify-between">
-          <MdNightsStay />
-          <MdWbSunny />
+        <h1 className="text-lg">Chetan Banjara</h1>
+        <div
+          onClick={() => {
+            setDarkMode(!darkMode);
+          }}
+        >
+          {darkMode ? (
+            <MdWbSunny
+              className=" 
+            text-2xl
+            cursor-pointer"
+            />
+          ) : (
+            <MdNightsStay className="text-2xl cursor-pointer" />
+          )}
         </div>
       </nav>
     </header>
