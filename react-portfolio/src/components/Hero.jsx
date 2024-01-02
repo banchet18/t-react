48c3638f5e9d4dd055711e86bb04360cd0d51133
banchet18 <banjarac6@gmail.com>
2024-01-02 14:14:46 +0530

    c

--- react-portfolio/src/components/Hero.jsx
+++ react-portfolio/src/components/Hero.jsx
@@ -2,6 +2,22 @@ import { FaTwitter, FaFacebook, FaLinkedin, FaArrowDown } from "react-icons/fa";
 import avatar from "../assets/avatar.png";
 
 const Hero = () => {
-  return <div></div>;
+  const SOCIAL = [
+    { id: 1, link: "sad", icon: <FaTwitter /> },
+    { id: 1, link: "sad", icon: <FaFacebook /> },
+    { id: 1, link: "sad", icon: <FaLinkedin /> },
+  ];
+
+  return (
+    <section>
+      <h2>Chetan </h2>
+      <h3>Software engineer</h3>
+      <p>
+        hello <span className="animate-pulse text-4xl">👋</span>
+        welcome to site, I am a freelace software engineer. I love to work on
+        web dev, Mobile dev & automation projects.
+      </p>
+    </section>
+  );
 };
 export default Hero;
