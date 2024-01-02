c675515f076e32b98ca1a9d2ad67d88e1effdd0e
banchet18 <banjarac6@gmail.com>
2024-01-02 12:55:27 +0530

    c

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
 const Header = () => {
   return (
-    <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <nav className="flex justify-between">
+    <header className="bg-white dark:bg-gray-900  text-gray-900 dark:text-white">
+      <nav className="flex justify-between items-center p-5">
         <h1>Jarvis</h1>
         <div className="flex justify-between">
           <MdNightsStay />
