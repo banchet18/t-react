188d764f90ee73522cd0dce5a8e5dc99ecb07349
banchet18 <banjarac6@gmail.com>
2024-01-02 21:48:46 +0530

    c

--- react-portfolio-practice/src/App.jsx
+++ react-portfolio-practice/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>hello</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element=<Home /> />
+        <Route path="/projects" element=<Projects /> />
+        <Route path="/experience" element=<Experience /> />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
