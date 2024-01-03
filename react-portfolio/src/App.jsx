b1e70085eb183da0a81c22ce223aad6a8ef4c7b7
banchet18 <banjarac6@gmail.com>
2024-01-03 06:38:35 +0530

    c

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
