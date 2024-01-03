75430f0b50cded7910e5c5f07bf4bf2d039a15f3
banchet18 <banjarac6@gmail.com>
2024-01-03 07:49:40 +0530

    c

--- react-portfolio/src/components/Main.jsx
+++ react-portfolio/src/components/Main.jsx
@@ -1,5 +1,7 @@
 import "../styles/Main.css";
 import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import avatar from "../assets/avatar.jpg";
 
 const Main = () => {
   return (
@@ -7,9 +9,18 @@ const Main = () => {
       <div className="hero">
         <h1>Hi, My Name is Chetan Banjara</h1>
         <p>A software developer with a passion for learning and creating.</p>
-        <FaLinkedin />
+        <div>
+          <a href="https://www.linkedin.com/feed/" target="blank">
+            <FaLinkedin />
+          </a>
+          <a href="" target="blank">
+            <MdEmail />
+          </a>
+        </div>
       </div>
-      <div className="heroimg"></div>
+      <div className="heroimg">
+        <img src={avatar} alt="" />
+      </div>
     </div>
   );
 };
