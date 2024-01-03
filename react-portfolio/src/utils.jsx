51b926ded580dccbc6bdaa1dd16ea1bb032fe865
banchet18 <banjarac6@gmail.com>
2024-01-03 08:09:35 +0530

    c

--- react-portfolio/src/utils.jsx
+++ react-portfolio/src/utils.jsx
@@ -1,4 +1,15 @@
-const ProjectList = () => {
-  return <div>ProjectList</div>;
-};
+const ProjectList = [
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png",
+    link: "https://www.google.com/",
+  },
+  {
+    img: "https://www.facebook.com/images/fb_icon_325x325.png",
+    link: "https://www.facebook.com/",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGIdPDUXpjMAZIXyl_O70iA-dIyjivp4agNw&usqp=CAU",
+    link: "https://twitter.com/i/flow/login",
+  },
+];
 export default ProjectList;
