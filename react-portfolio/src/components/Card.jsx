4e587d3049ee77a5d4a37b0b32876f5ccfc3baef
banchet18 <banjarac6@gmail.com>
2024-01-03 08:13:45 +0530

    c

---
+++ react-portfolio/src/components/Card.jsx
@@ -0,0 +1,6 @@
+import "../styles/Card.css";
+
+const Card = () => {
+  return <div className="cardmain"></div>;
+};
+export default Card;
--- react-portfolio/src/pages/Projects.jsx
+++ react-portfolio/src/pages/Projects.jsx
@@ -1,10 +1,9 @@
+import Card from "../components/Card";
+
 const Projects = () => {
   return (
     <div>
-      <img
-        src="https://logos.textgiraffe.com/logos/logo-name/Chetan-designstyle-smoothie-m.png"
-        alt=""
-      />
+      <Card />
     </div>
   );
 };
