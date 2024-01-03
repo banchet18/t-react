78ab488d38e25071cdf7363820fb9d045a524bfa
banchet18 <banjarac6@gmail.com>
2024-01-03 07:35:23 +0530

    c

--- react-portfolio/src/components/Main.jsx
+++ react-portfolio/src/components/Main.jsx
@@ -1,6 +1,14 @@
 import "../styles/Main.css";
 
 const Main = () => {
-  return <div className="main">Main</div>;
+  return (
+    <div className="main">
+      <div className="hero">
+        <h1>Hi, My Name is Chetan Banjara</h1>
+        <p>A software developer with a passion for learning and creating.</p>
+      </div>
+      <div className="heroimg"></div>
+    </div>
+  );
 };
 export default Main;
